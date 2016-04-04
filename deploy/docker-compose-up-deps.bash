0dbc065eba5c00c1b4df6c9e2b1ee5f410ee2130
Roy Jung <phi4grv@gmail.com>
2016-04-05 00:17:58 +1000

    Use docker-compose-deps.yml

--- deploy/docker-compose-up-deps.bash
+++ deploy/docker-compose-up-deps.bash
@@ -13,5 +13,11 @@ script_dir() {
 }
 
 SCRIPT_DIR=$( script_dir )
+ROOT_DIR=$( dirname $SCRIPT_DIR )
 
-docker-compose -f ${DOCKER_COMPOSE_FILE} "$@"
+DOCKER_COMPOSE_FILE=${SCRIPT_DIR}/docker-compose-deps.yml
+
+docker-compose -f ${DOCKER_COMPOSE_FILE} up -d db
+sleep 2
+docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm auth-api bin/rails db:setup
+docker-compose -f ${DOCKER_COMPOSE_FILE} up -d auth-api
