08c0e09096befa99344adbf6cd586f4589dfc83e
Roy Jung <phi4grv@gmail.com>
2016-04-05 00:08:17 +1000

    Get default url host from ENV

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.application.routes.default_url_options[:host] = 'users-api.dev'
+Rails.application.routes.default_url_options[:host] =
+  "#{ENV['USERS_API_EXTERNAL_HOST']}:#{ENV['USERS_API_EXTERNAL_PORT']}"
