a88c649630842be783eac80f778c25692ce2ad28
Roy Jung <phi4grv@gmail.com>
2016-04-13 17:01:37 +1000

    Add token is issued fixture helper

--- spec/support/features/token.rb
+++ spec/support/features/token.rb
@@ -15,13 +15,42 @@ module Features
       JSON.parse(response.body).with_indifferent_access
     end
 
-    def token_should_be_valid(token:)
+    def token_is_issued(token: nil, user:)
+      token ||= token_is_granted_by_client_credentials users_api_test_client
+      account = find_or_create_account user: user, token: token
+      issue_token account: account, token: token
+    end
+
+    def find_or_create_account(user:, token:)
+      response = auth_api_conn.post do |req|
+        req.url '/v1/test/user-accounts'
+        req.headers['Authorization'] = "Bearer #{token[:access_token]}"
+        req.body = { attrs_set: [ parti: { identifier: user.identifier } ] }
+      end
+      JSON.parse(response.body).first.with_indifferent_access
+    end
+
+    def issue_token(account:, token:)
+      response = auth_api_conn.post do |req|
+        req.url "/v1/test/user-accounts/#{account[:identifier]}/tokens"
+        req.headers['Authorization'] = "Bearer #{token[:access_token]}"
+      end
+      JSON.parse(response.body).with_indifferent_access
+    end
+
+    def token_should_be_valid(token:, **rest)
       response = auth_api_conn.post do |req|
         req.url '/v1/introspect'
-        req.headers['Authorization'] = "Bearer #{token}"
-        req.body = { token: token }
+        req.headers['Authorization'] = "Bearer #{token[:access_token]}"
+        req.body = { token: token[:access_token] }
       end
       expect(response.status).to eq(200)
+      body = JSON.parse(response.body).with_indifferent_access
+      if rest[:user]
+        expect(body[:grant_type]).to eq('authorization_code')
+        expect(body[:connect_type]).to eq('parti')
+        expect(body[:connect_id]).to eq(rest[:user].identifier)
+      end
     end
 
     def auth_api_conn
--- spec/support_spec/features/token_spec.rb
+++ spec/support_spec/features/token_spec.rb
@@ -3,11 +3,22 @@ require 'rails_helper'
 describe Features::Token do
   include Features::Client
   include Features::Token
+  include Features::User
 
   describe 'token_is_granted_by_client_credentials' do
     it 'grants token by client_credentials' do
       token = token_is_granted_by_client_credentials users_api_test_client
-      token_should_be_valid token: token[:access_token]
+      token_should_be_valid token: token
+    end
+  end
+
+  describe 'token_is_issued' do
+    it 'issues token for user' do
+      user = user_exists
+
+      token = token_is_issued user: user
+
+      token_should_be_valid token: token, user: user
     end
   end
 end
