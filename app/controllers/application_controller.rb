16563eaf80717c22b5e106ae5e84551a14172928
Roy Jung <phi4grv@gmail.com>
2016-04-13 23:07:08 +1000

    Add test for get user unhappy case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::API
   include Concerns::Authentication
 
   rescue_from ActionController::ParameterMissing, :with => :bad_request
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   def bad_request(error)
     render json: { error: error.message }, status: :bad_request
+  end
+
+  def record_not_found(error)
+    render json: { error: error.message }, status: :not_found
   end
 end
--- spec/features/get_user_spec.rb
+++ spec/features/get_user_spec.rb
@@ -11,4 +11,22 @@ describe 'Get user endpoint' do
 
     user_should_be_rendered user
   end
+
+  it 'responds 404 not found with not existing user' do
+    user = user_exists
+    token = token_is_issued user: user
+
+    user.identifier = 'not-exist-user-identifier'
+    request_get_user user: user, token: token
+
+    response_should_be_404_not_found
+  end
+
+  it 'responds 401 unauthorized without token' do
+    user = user_exists
+
+    request_get_user user: user
+
+    response_should_be_401_unauthorized
+  end
 end
