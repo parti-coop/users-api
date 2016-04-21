617419a1a5a1ab2aeaf4ae12f1a07c50db9a200f
Roy Jung <phi4grv@gmail.com>
2016-04-21 14:59:11 +1000

    Use DB_HOST env in Dockerfile command

--- deploy/docker-cmd.sh
+++ deploy/docker-cmd.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-dockerize -wait tcp://users-db:5432
+DB_HOST=${DB_HOST:-users-db}
+dockerize -wait tcp://${DB_HOST}:5432
 
 if [ -f /volume/shared/is_leader ]; then
     bin/rake db:migrate
